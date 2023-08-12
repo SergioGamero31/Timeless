6c0017a05c5236e8bca9482e2002abd1ef03bf2f
Sergio Gamero <andre25685@gmail.com>
2023-08-12 00:54:14 -0500

    perf: Optimize images to improve performance

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,9 +1,9 @@
 <template>
     <Splide :options="{ rewind: true, arrows: false }" tag="section"
     class="rounded-3xl overflow-hidden">
-        <SplideSlide v-for="slide in slides" class="flex items-center hover:cursor-grab sm:h-[40%] lg:h-[40vh] active:cursor-grabbing">
-            <img class="w-full object-cover" :src="slide.img">
-            <component :is="slide.component" class="absolute self-end sm:m-5 md:m-10 sm:w-[45%] lg:w-[22%] sm:text-2xl lg:text-4xl text-slate-100 uppercase drop-shadow-[0_5px_5px_rgba(0,0,0,0.7)]">
+        <SplideSlide v-for="slide in slides" class="relative hover:cursor-grab sm:h-[25vh] lg:h-[40vh] active:cursor-grabbing">
+            <img class="h-full w-full object-cover" :class="slide.position" :src="slide.img" alt="Fotografía del modelo">
+            <component :is="slide.component" class="absolute my-auto bottom-0 sm:m-5 md:m-10 sm:w-[45%] lg:w-[22%] sm:text-2xl lg:text-4xl text-slate-100 uppercase drop-shadow-[0_5px_5px_rgba(0,0,0,0.7)]">
                 {{ slide.quote }}
             </component>
         </SplideSlide>
@@ -19,16 +19,19 @@ const slides = [
         quote: 'moda atemporal para expresar tu esencia',
         component: 'h1',
         img: 'src/assets/img/slide1.webp',
+        position: 'object-top'
     },
     {
         quote: 'detalles que transforman tu estilo',
         component: 'span',
         img: 'src/assets/img/slide2.webp',
+        position: 'object-center'
     },
     {
         quote: 'Refleja tu fuerza y estilo',
         component: 'span',
         img: 'src/assets/img/slide3.webp',
+        position: 'object-center'
     }
 ]
 </script>
