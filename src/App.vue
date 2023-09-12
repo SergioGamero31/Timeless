78457c16f03fbe366f776fd9c75c06b85b40ee1a
Sergio Gamero <andre25685@gmail.com>
2023-09-11 21:36:04 -0500

    feat: Add SideBar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
-  <div class="bg-dun h-screen sm:px-5 md:px-8 lg:px-24 py-10 overflow-auto text-">
+  <Transition name="fade">
+    <SideBar @sideBar="toogleSideBar" v-if="sideBar" 
+    class="fixed h-screen w-2/5 z-10 bg-linen"/>
+  </Transition>
+  <div class="flex flex-col gap-7 sm:px-5 md:px-8 lg:px-24 pt-10 pb-5">
     <header class="flex justify-between">
-      <NavBar/>
-      <div class="flex w-1/4 gap-3">
-        <SearchBar/>
-        <CartButton class="bg-khaki-400 hover:bg-khaki-500 transition-colors text-eerie-black" aria-label="Ver carrito"/>
-      </div>
+      <NavBar @sideBar="toogleSideBar"/>
+      <CartButton class="bg-khaki-400 hover:bg-khaki-500 transition-colors text-eerie-black" aria-label="Ver carrito"/>
     </header>
-    <RouterView />
+    <RouterView/>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
-import SearchBar from './components/SearchBar.vue'
 import CartButton from './components/CartButton.vue'
+import SideBar from './components/SideBar.vue';
+
+const sideBar = ref(false)
+
+const toogleSideBar = () => {
+  sideBar.value = !sideBar.value
+}
 </script>
+
+<style>
+html{
+  background-color: #D8C4B6;
+}
+.fade-enter-active {
+  animation: fadeInRight 0.2s ease-in;
+}
+.fade-leave-active {
+  animation: fadeOutLeft 0.2s ease-in-out;
+}
+
+@keyframes fadeInRight {
+  from {
+    transform: translateX(-10rem);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+@keyframes fadeOutLeft {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-10rem);
+  }
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col items-end p-5 ">
+        <button @click="$emit('sideBar')" aria-label="Cerrar menu"><CloseIcon/></button>
+        <nav class="self-start">
+            <ul class="flex flex-col font-medium gap-4">
+                <li @click="$emit('sideBar')"><RouterLink :to="{ name: 'home' }">Inicio</RouterLink></li>
+                <li @click="$emit('sideBar')"><RouterLink :to="{ name: 'men' }">Hombre</RouterLink></li>
+                <li @click="$emit('sideBar')"><RouterLink :to="{ name: 'women' }">Mujer</RouterLink></li>
+                <li @click="$emit('sideBar')"><RouterLink :to="{ name: 'jewelery' }">Joyería</RouterLink></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+import CloseIcon from '~icons/mingcute/close-line'
+</script>
