bd09244c9e960cafb9a16938e2a18e3da16c9ccc
Sergio Gamero <andre25685@gmail.com>
2023-09-11 21:40:16 -0500

    feat: Update NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,15 @@
 <template>
-    <nav class="flex gap-8 text-eerie-black">
-        <p class="font-bold uppercase">Timeless</p>
-        <ul class="flex font-medium gap-8">
-            <li>Hombre</li>
-            <li>Mujer</li>
-            <li>Accesorios</li>
+    <nav class="flex sm:gap-3 lg:gap-8 items-center text-eerie-black">
+        <button @click="$emit('sideBar')" class="lg:hidden" aria-label="Abrir menu"><MenuIcon/></button>
+        <RouterLink to="/" class="font-bold uppercase">Timeless</RouterLink>
+        <ul class="sm:hidden lg:flex font-medium gap-8">
+            <li><RouterLink :to="{ name: 'men' }">Hombre</RouterLink></li>
+            <li><RouterLink :to="{ name: 'women' }">Mujer</RouterLink></li>
+            <li><RouterLink :to="{ name: 'jewelery' }">Joyería</RouterLink></li>
         </ul>
     </nav>
 </template>
 
 <script setup>
+import MenuIcon from '~icons/mingcute/menu-line'
 </script>
