cee949db05c4e4d713c42eeef6cdeeca8342d9aa
Sergio Gamero <andre25685@gmail.com>
2023-09-11 21:23:47 -0500

    style: Update carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,9 +1,9 @@
 <template>
     <Splide :options="{ rewind: true, arrows: false }" tag="section"
     class="rounded-3xl overflow-hidden">
-        <SplideSlide v-for="slide in slides" class="relative hover:cursor-grab sm:h-[25vh] md:h-[30vh] lg:h-[40vh] active:cursor-grabbing">
+        <SplideSlide v-for="slide in slides" class="relative hover:cursor-grab sm:h-[35vh] md:h-[40vh] active:cursor-grabbing">
             <img class="h-full w-full object-cover" :class="slide.position" :src="slide.img" alt="Fotografía del modelo">
-            <component :is="slide.component" class="absolute my-auto bottom-0 sm:m-5 md:m-10 sm:w-[45%] lg:w-[22%] sm:text-2xl lg:text-4xl text-slate-100 uppercase drop-shadow-[0_5px_5px_rgba(0,0,0,0.7)]">
+            <component :is="slide.component" class="absolute my-auto bottom-0 sm:m-5 md:m-10 sm:w-[60%] lg:w-[22%] sm:text-2xl lg:text-4xl text-slate-100 uppercase drop-shadow-[0_5px_5px_rgba(0,0,0,0.7)]">
                 {{ slide.quote }}
             </component>
         </SplideSlide>
