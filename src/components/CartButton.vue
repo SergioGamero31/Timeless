32cb2ec000762ff8cfa37d25df6b467c557d2a4c
Sergio Gamero <andre25685@gmail.com>
2023-09-15 03:09:25 -0500

    feat: Add cart status indicator

--- src/components/CartButton.vue
+++ src/components/CartButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="relative bg-khaki-400 hover:bg-khaki-500 transition-colors text-eerie-black rounded-xl p-2" aria-label="Ver carrito">
-        <div v-if="store.isCartEmpty">
+        <div v-if="!store.isCartEmpty">
             <span class="absolute bottom-7 right-0 bg-brick-red-600 rounded-full h-2.5 w-2.5 animate-ping"></span>
             <span class="absolute bottom-7 right-0 bg-brick-red-600 rounded-full h-2.5 w-2.5"></span>
         </div>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -4,19 +4,27 @@ export const useCartStore = defineStore('cart', {
     state: ()=>{
         return {
             cart: [],
+            isCartEmpty: true
         }
     },
     actions: {
         addToCart(product){
             const existingCartItem = this.cart.find(item => item.product.id === product.id)
             if(existingCartItem) existingCartItem.quantity++
-            else this.cart.push({ product, quantity: 1 })
+            else {
+                this.cart.push({ product, quantity: 1 })
+            }
+            this.checkCartStatus()
         },
         removeFromCart(product){
             const productIndex = this.cart.findIndex(item => item.product.id === product.product.id)
             if(productIndex !== -1){
                 this.cart.splice(productIndex, 1)
             }
+            this.checkCartStatus()
+        },
+        checkCartStatus(){
+            this.isCartEmpty = this.cart.length === 0
         }
     }
 })
